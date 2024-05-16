717e3de97565bdc9ba5ba729886c03d5aeef08fb
Ayad Masud <ayad.r.masud@gmail.com>
2024-05-15 21:23:14 -0500

    integrated summarizer module

--- page.py
+++ page.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import summarizer
 
 app = Flask(__name__)
+link = []
 
-test = summarizer.summarize(summarizer.reviews, summarizer.model)
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def load():
-    return render_template("index.html", test=test)
+    if request.method == "POST":
+        link.append(request.form.get('gate'))
+    return render_template("index.html") # this is blank flask page with empty form and question
 
 
-app.run()
+
+@app.route('/summary')
+def summary():
+    test = summarizer.summarize(link[0], summarizer.model)
+    return render_template("results.html", results=test)
+
+
+if __name__ == '__main__':
+    app.run()
+
+# feed link from this file
