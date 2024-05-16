3e9eeb0a1bcf7f1897838a607566f123b40dcec3
Ayad Masud <ayad.r.masud@gmail.com>
2024-05-15 20:47:31 -0500

    integrating link function

--- summarizer.py
+++ summarizer.py
@@ -3,24 +3,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import google.generativeai as palm
 import key
-import page
 
 
 
-# for getting reviews from website
-driver = webdriver.Firefox() # which browser it opens
-driver.implicitly_wait(1) # wait for elements to load in page
-driver.get(page.link[0]) # currently manual link, but prof search bar?
-driver.find_element(By.XPATH, "/html/body/div[5]/div/div/button").click() # find pop up button for cookies and click to exit
-driver.implicitly_wait(1)
-elements = driver.find_elements(By.CLASS_NAME, "gRjWel") # list of the reviews
+def reviews(link):
+    # for getting reviews from website
+    driver = webdriver.Firefox() # which browser it opens
+    driver.implicitly_wait(1) # wait for elements to load in page
+    driver.get(link) # currently manual link, but prof search bar?
+    driver.find_element(By.XPATH, "/html/body/div[5]/div/div/button").click() # find pop up button for cookies and click to exit
+    driver.implicitly_wait(1)
+    elements = driver.find_elements(By.CLASS_NAME, "gRjWel") # list of the reviews
 
-# creates a list and get the actual text from the html elements stored in elements
-reviews = []
-for element in elements:
-    reviews.append(element.text)
+    # creates a list and get the actual text from the html elements stored in elements
+    reviews = []
+    for element in elements:
+        reviews.append(element.text)
 
-driver.close() # closes the window 
+    driver.close() # closes the window 
+    return reviews
 
 # configuring key
 palm.configure(api_key=key.API_KEY)
@@ -30,12 +31,12 @@ models = [m for m in palm.list_models() if 'generateText' in m.supported_generat
 model = models[0].name
 
 # function that uses model and scraped data
-def summarize(reviews, model):
+def summarize(link, model):
     # prompt to feed into LLM, which then uses the reviews according to what the 
     prompt = "Take these reviews of professors from students and write me a list of the best and worst things about this professor."
-
+    review = reviews(link)
     # adding each review and prompt into one string to feed to the LLM
-    for element in reviews:
+    for element in review:
         prompt += "\n" + element
 
     # LLM parameters 
@@ -53,8 +54,8 @@ def formatting(para):
     pass
 
 # call function
-#print(summarize(reviews, model))
-test = summarize(reviews, model)
+print(summarize("https://www.ratemyprofessors.com/professor/1869654", model))
+
 
 
 
